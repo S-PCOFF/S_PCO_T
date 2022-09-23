80a554311513222e2a98bfd428ef12467745e670
ShinhanDS <dspcoff@naver.com>
2022-09-23 17:55:42 +0900

    travis s3 연동 V5

---
+++ src/main/java/com/shds/pco/HelloController2.java
@@ -0,0 +1,13 @@
+package com.shds.pco;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController2 {
+    @GetMapping("/hello2")
+    public String hello(){
+        return "hello";
+    }
+}
