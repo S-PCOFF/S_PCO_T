d5d0852bbf132374f6c8ed45a3d3d3ba9a7e97e6
ShinhanDS <dspcoff@naver.com>
2022-09-23 17:38:46 +0900

    travis s3 연동 V3

--- src/main/java/com/shds/pco/HelloController.java
+++ src/main/java/com/shds/pco/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello_v2";
+        return "hello_v3";
     }
 }
