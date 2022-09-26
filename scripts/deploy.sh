bb9c3d21a94347b68aa3290d13453eaa54a1eb06
ShinhanDS <dspcoff@naver.com>
2022-09-26 17:46:51 +0900

    배포자동화구성V3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -40,6 +40,9 @@ echo "> $JAR_NAME 실행"
 #    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 nohup java -jar \
--Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
--Dspring.profiles.active=real \
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+#nohup java -jar \
+#-Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+#-Dspring.profiles.active=real \
+#$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
