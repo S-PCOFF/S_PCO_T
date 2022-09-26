f51b6367e2d7c8cd4f81cae19fb75c7b9befefbb
ShinhanDS <dspcoff@naver.com>
2022-09-26 17:58:53 +0900

    배포자동화구성 테스트2

--- src/main/java/com/shds/pco/HelloController2.java
+++ src/main/java/com/shds/pco/HelloController2.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController2 {
     @GetMapping("/hello2")
     public String hello(){
-        return "hello!!! 2번째 버전";
+        return "hello!!! 2번째 버전 입니다.";
     }
 }
