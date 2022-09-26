d79a1584cbe06377fa54805dd18e8a68b736e0fe
ShinhanDS <dspcoff@naver.com>
2022-09-26 18:33:39 +0900

    배포자동화구성 테스트8

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=S_PCO
+PROJECT_NAME=S_PCO_T
 
 echo "> Build 파일 복사"
 
@@ -11,7 +11,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
