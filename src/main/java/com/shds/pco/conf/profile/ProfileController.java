fb996dc4fb459899f42d8af5264773998d4c98ac
ShinhanDS <dspcoff@naver.com>
2022-09-28 11:10:30 +0900

    nginx 세팅1

---
+++ src/main/java/com/shds/pco/conf/profile/ProfileController.java
@@ -0,0 +1,34 @@
+package com.shds.pco.conf.profile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/profile")
+public class ProfileController {
+
+    //@Autowired
+    private final Environment environment;
+
+    @GetMapping
+    public String profile() {
+        // .getActiveProfiles() : 현재 실행 중인 Active Profile 목록 조회
+        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
