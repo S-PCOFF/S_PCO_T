61eb64aaee304fc8be2322640fec2360b1aabbf8
ShinhanDS <dspcoff@naver.com>
2022-09-28 11:21:39 +0900

    nginx 다시 세팅1

--- src/main/java/com/shds/pco/HelloController2.java
+++ src/main/java/com/shds/pco/HelloController2.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController2 {
     @GetMapping("/hello2")
     public String hello(){
-        return "hello!!! 버전4";
+        return "hello!!! nginx 버전1";
     }
 }
