b4080440eb2dbfb9645da3cc01832e51801d8d91
ShinhanDS <dspcoff@naver.com>
2022-09-28 16:05:40 +0900

    무중단배포 테스트7

--- src/main/java/com/shds/pco/HelloController2.java
+++ src/main/java/com/shds/pco/HelloController2.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController2 {
     @GetMapping("/hello2")
     public String hello(){
-        return "hello!!! 무중단 배포 테스트6";
+        return "hello!!! 무중단 배포 테스트7";
     }
 }
