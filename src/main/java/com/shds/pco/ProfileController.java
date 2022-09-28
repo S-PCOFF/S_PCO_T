e77a4ca7fadc2aa6be130cff4c4fca4145763b04
ShinhanDS <dspcoff@naver.com>
2022-09-28 17:55:03 +0900

    무중단배포 테스트10

--- src/main/java/com/shds/pco/ProfileController.java
+++ src/main/java/com/shds/pco/ProfileController.java
@@ -1,7 +1,6 @@
 package com.shds.pco;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,29 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
+    private final Environment env;
 
-    private Environment env;
-
-    @Autowired
-    public ProfileController(Environment env) {
-        this.env = env;
-    }
-
-    @GetMapping(value = "/profile")
+    @GetMapping("/profile")
     public String profile() {
-        // .getActiveProfiles() : 현재 실행 중인 Active Profile 목록 조회
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
                 .orElse(defaultProfile);
     }
-
 }
