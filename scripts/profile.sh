d5d7847d46b0aa69db9fa0fc462eb1afa0ae5bea
ShinhanDS <dspcoff@naver.com>
2022-09-28 18:52:16 +0900

    무중단배포 쉘 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -8,29 +8,29 @@ function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
+    if [ ${RESPONSE_CODE} -ge 400 ]
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        CURRENT_PROFILE=real2
+        CURRENT_PROFILE=set2
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
-
-    if [ ${CURRENT_PROFILE} == real1 ]
+    if [ ${CURRENT_PROFILE} == set1 ]
     then
-      IDLE_PROFILE=real2
+      IDLE_PROFILE=set2
+    elif [ ${CURRENT_PROFILE} == set2 ]
+    then
+      IDLE_PROFILE=set1
     else
-      IDLE_PROFILE=real1
+      IDLE_PROFILE=set1
     fi
-
     echo "${IDLE_PROFILE}"
 }
-
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
-
-    if [ ${IDLE_PROFILE} == real1 ]
+    if [ ${IDLE_PROFILE} == set1 ]
     then
       echo "8081"
     else
