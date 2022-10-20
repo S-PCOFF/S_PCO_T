b2c3faeb727f280d8f25e476c653e8573106f57c
ShinhanDS <dspcoff@naver.com>
2022-10-20 15:17:30 +0900

    docker 연동 테스트11

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,13 +6,13 @@
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://3.37.219.183/profile)
+    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://3.37.219.183/profile/)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(sudo curl -s http://3.37.219.183/profile)
+        CURRENT_PROFILE=$(sudo curl -s http://3.37.219.183/profile/)
     fi
 
     if [ "${CURRENT_PROFILE}" == "real1" ]
